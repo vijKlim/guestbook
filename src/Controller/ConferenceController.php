d1e55fe1200678b1cd327a734f191c34492ef674
Vitalii <vij.klimenko@gmail.com>
2021-01-16 21:49:21 +0100

    Added TwigEventSubscriber

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -31,12 +31,11 @@ class ConferenceController extends AbstractController
     /**
     * @Route("/conference/{id}", name="conference")
     */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return  new Response($this->twig->render('conference/show.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
